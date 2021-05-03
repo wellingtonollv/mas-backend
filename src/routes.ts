8bebc7b2cd91ace5b455b29dcde5bfc4f37dbf28
Wellington Oliveira <28570773+wellingtonollv@users.noreply.github.com>
2021-05-02 21:44:33 -0300

    fix exercises

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,15 @@
-import {Router} from 'express';
+import { Router } from "express"
 
-const routes = Router();
+const routes = Router()
 
-routes.get('/user', (request, response) => response.json({
-    message: 'Hello World'
-}))
+routes.post("/user", (request, response) => {
+	const { name, email, password } = request.body
+	const user = {
+		name,
+		email,
+		password,
+	}
+	return response.json({user})
+})
 
-export default routes;
+export default routes
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
-
+import './database';
+    
 const app = express();
 
 app.use(cors());
