794f85d5a1d1831c4495134be6744b3e0accb35e
Wellington Oliveira <28570773+wellingtonollv@users.noreply.github.com>
2021-05-28 00:24:15 -0300

    route change

--- src/routes.ts
+++ src/routes.ts
@@ -1,23 +1,35 @@
-import { Router } from "express"
-import {UserController} from './controller/UserController';
-import {ActivyController} from './controller/ActivyController';
-import {CourseUnitController} from './controller/CourseUnityController';
-import {AuthenticateController } from './controller/AuthenticateController';
-import authenticated from './middlewares/authenticated';
+import { Request, Response, Router } from "express"
+import { UserController } from "./controller/UserController"
+import { ActivyController } from "./controller/ActivyController"
+import { CourseUnitController } from "./controller/CourseUnityController"
+import { AuthenticateController } from "./controller/AuthenticateController"
+import authenticated from "./middlewares/authenticated"
 
+const userController = new UserController()
+const activyController = new ActivyController()
+const courseUnitController = new CourseUnitController()
+const authenticateController = new AuthenticateController()
 
-const userController = new UserController();
-const activyController = new ActivyController();
-const courseUnitController = new CourseUnitController();
-const authenticateController = new AuthenticateController();
+const routes = Router()
 
-const routes = Router();
+routes.post("/user", authenticated, userController.create)
 
-
-routes.post('/user', authenticated, userController.create);
-routes.post('/auth', authenticateController.create);
-routes.post('/activy', authenticated, activyController.create);
-routes.post('/courseunit', authenticated, courseUnitController.create);
-
+routes.post('/user', (request: Request, response: Response) => {
+    const {name, email, password} = request.body
+    const user = {
+    name,
+    email,
+    password
+    }
+    return response.json(user);
+    })
+routes.get('/user/', (req: Request, resp: Response) => {
+    return resp.json({ 
+        message: 'erro'
+    })
+})
+routes.post("/auth", authenticateController.create)
+routes.post("/activy", authenticated, activyController.create)
+routes.post("/courseunit", authenticated, courseUnitController.create)
 
 export default routes
